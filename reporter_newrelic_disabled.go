50fe66ebf2ee05d6201567715b2705489aa8a91e
Sergey Lanzman <Sergeylanz@gettaxi.com>
2015-11-06 14:08:21 +0200

    add custom metric report

--- reporter_newrelic_disabled.go
+++ reporter_newrelic_disabled.go
@@ -7,3 +7,7 @@ type NRTxReporter struct{}
 func (r *NRTxReporter) ReportError(txnID int64, exceptionType, errorMessage, stackTrace, stackFrameDelim string) (int, error) {
 	return 0, nil
 }
+
+func (t *NRTxReporter) SetCustomMetric(name string, value float64) (int, error) {
+	return 0, nil
+}
--- reporter_newrelic_enabled.go
+++ reporter_newrelic_enabled.go
@@ -11,3 +11,7 @@ type NRTxReporter struct{}
 func (r *NRTxReporter) ReportError(txnID int64, exceptionType, errorMessage, stackTrace, stackFrameDelim string) (int, error) {
 	return sdk.TransactionNoticeError(txnID, exceptionType, errorMessage, stackTrace, stackFrameDelim)
 }
+
+func (t *NRTxReporter) SetCustomMetric(name string, value float64) error {
+	return sdk.RecordMetric(name, value)
+}
